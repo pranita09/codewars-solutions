8f78f5bffc794fc9830460cc538fc7757ed096c5
Pranita <pfulsundar8@gmail.com>
2023-02-23 19:38:47 +0530

    refactor: added 3rd question

--- fundamentals-solutions/sol3.js
+++ fundamentals-solutions/sol3.js
@@ -58,4 +58,9 @@ const getSum1 = (a, b) => {
 
 
 
-// Question 03: 
+// Question 03: Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case. (In this case, all triangles must have surface greater than 0 to be accepted).
+
+const isTriangle = (a, b, c) => a + b > c && a + c > b && c + b > a;
+console.log(isTriangle(10, -2, 10))
+
+// Question 04: 
