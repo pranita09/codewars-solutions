3fd19e43382620f3cc76210d9461decddf1db2a8
Pranita <pfulsundar8@gmail.com>
2023-02-23 19:22:14 +0530

    refactor: added 2nd question

--- fundamentals-solutions/sol3.js
+++ fundamentals-solutions/sol3.js
@@ -28,3 +28,34 @@ function accum2(s){
 
 // console.log(accum1("Alka"));
 
+
+// Question 02: Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b. Note: a and b are not ordered!
+
+const getSum = (a, b) => {
+let sum = 0;
+  if(a<b){
+    for(let i=a; i<=b; i++){
+      sum += i;
+    }
+    return sum;
+  }else{
+    for(let i=b; i<=a; i++){
+      sum+= i;
+    }
+    return sum;
+  }  
+}
+
+const getSum1 = (a, b) => {
+    let sum = 0;
+    let bigger = a>b ? a : b;
+    let smaller = a<b ? a : b;
+    sum = (bigger+smaller)*(bigger-smaller+1)/2;
+    return sum;
+}
+
+// console.log(getSum1(12, 2));
+
+
+
+// Question 03: 
