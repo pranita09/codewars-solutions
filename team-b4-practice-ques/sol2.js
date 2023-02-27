7f140b8beb1e4f621879f68982944ac24993b1ed
Pranita <pfulsundar8@gmail.com>
2023-02-27 12:02:01 +0530

    fix: function

--- team-b4-practice-ques/sol2.js
+++ team-b4-practice-ques/sol2.js
@@ -2,7 +2,7 @@
 // You are given a string that contains consecutive words that are same, return a string with all the repeating consecutive words removed.
 
 
-const strInput = "hey hey you you have a a nice day day hey"
+const strInput = "hey hey hey you you have a a nice day day hey"
 
 const removeRepeatingWords = strInput => {
     let arrayOfWords = strInput.split(" ");
@@ -10,6 +10,7 @@ const removeRepeatingWords = strInput => {
     for(let i=0; i<arrayOfWords.length; i++){
         if(arrayOfWords[i]===arrayOfWords[i+1]){
             arrayOfWords.splice(i, 1);
+            i--;
         }
     }
     console.log(arrayOfWords);
--- team-b4-practice-ques/sol3.js
+++ team-b4-practice-ques/sol3.js
@@ -6,19 +6,32 @@ const strGiven = "abcdefgh";
 const arrGiven = [2,4,5];
 
 const indexedCapitalizer = (str, arr) => {
-    let newString = "";
-    for(let i=0; i<arr.length; i++){
-        for(let j=0; j<str.length; j++){
-            if(arr[i] === j){
-                newString+=str[j].toUpperCase();
-            }else{
-                newString+=str[j].toLowerCase();
+    let newArr = str.split('');
+    for(let i=0; i<newArr.length; i++){
+        for(let j=0; j<arr.length; j++){
+            if(i === arr[j]){
+                newArr[i] = newArr[i].toUpperCase();
             }
-            
         }
+    }
+    return newArr.join('');
+}
+
+const indexedCapitalizer1 = (str, arr) => {
+    let newString = "";
+    for(let i=0; i<str.length; i++){
+        for(var j=0; j<arr.length; j++){
+            if(i === arr[j]){
+                newString += str[i].toUpperCase();
+                break;
+            }
+        }
+        if(i !== arr[j])
+            newString += str[i];      
     }
     return newString;
 }
 
-console.log(indexedCapitalizer(strGiven,arrGiven));   // abCdefghabcdEfghabcdeFgh
+console.log(indexedCapitalizer1(strGiven,arrGiven))
+console.log(indexedCapitalizer1(strGiven,arrGiven));  
 //"abCdEFgh"
