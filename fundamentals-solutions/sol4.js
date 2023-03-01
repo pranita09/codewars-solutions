83ce698bce59c97a06adb8b951d97404138337d5
Pranita <pfulsundar8@gmail.com>
2023-03-01 13:58:07 +0530

    add: 4th question

--- fundamentals-solutions/sol4.js
+++ fundamentals-solutions/sol4.js
@@ -51,3 +51,23 @@ const sumOfSquares1 = arr => {
 // console.log(sumOfSquares1([1,2,2]));
 
 
+// Question 04:
+// Trolls are attacking your comment section! A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat. Your task is to write a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+function disemvowel(str) {
+    const vowels = ['a','e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let strWithoutVowels = "";
+    for(let i=0; i<str.length; i++){
+      if(!vowels.includes(str[i])){
+        strWithoutVowels += str[i];
+      }
+    }
+    return strWithoutVowels;
+  }
+
+// console.log(disemvowel("This website is for losers LOL!"));
+
+
+
+// Question 05:
+
